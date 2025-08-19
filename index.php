f754e0b7583b2b546da816b63d23dec58c7f3dd7
ayllaleticiasantos <jeleticiasantosdf@gmail.com>
2025-08-18 21:59:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// Aluno -> Aprovado ou Reprovado
+
+$n1=10;
+$n2=5;
+$n3=8;
+$n4=10;
+
+if($n1+$n2+$n3+$n4>=6){
+    echo "Aluno Está Aprovado!";
+}
+else{
+    echo"Aluno Está Reprovado!";
+}
